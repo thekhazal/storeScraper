8456dca7572d64313fb2a62631781b8891589fdd
thekhazal <anmarkhazal@gmail.com>
2020-11-15 20:53:13 +0100

    2 different methods, total store count differs tho

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # storeScraper
-Web scraper test
+Web scraper for Swedish grocery chain.
+
+This code has been created for training purposes.
+
+Store data is scraped using JSON request or BeautifulSoup, then converted to XLXS using Pandas.
--- scrape.py
+++ scrape.py
@@ -3,94 +3,65 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 
+def populateListOfDictionary(listOfDictionary, marketingName, visitingAddress, postCode, telephoneNumber ):
+    listOfDictionary.append(
+        {"MarketingName": marketingName,
+        "VisitingAddress": visitingAddress,
+        "PostCode": postCode,
+        "TelephoneNumber" : telephoneNumber})
+
 def solveByJson():
     jsonRequestUrl = 'https://www.ica.se/Templates/GlobalSearch/Handlers/GlobalSearchHandler.ashx?CommandName=GetAllStoresInProfile&Profile=all'
     jsonRequest = requests.get(jsonRequestUrl)
     jsonConversionToListOfDictionary = jsonRequest.json()
-    
+
     listOfDictionary = []
     for Store in jsonConversionToListOfDictionary:
-        listOfDictionary.append({"MarketingName": Store['MarketingName'],
-        "VisitingAddress": Store['VisitingAddress'],
-        "PostCode": Store['PostCode'],
-        "TelephoneNumber" : Store['TelephoneNumber']})
+        populateListOfDictionary(listOfDictionary,
+            Store['MarketingName'],
+            Store['VisitingAddress'],
+            Store['PostCode'],
+            Store['TelephoneNumber'])
 
-    pandas.DataFrame(listOfDictionary).to_excel('output.xlsx', header=False, index=False)
+    pandas.DataFrame(listOfDictionary).to_excel('solveByJson.xlsx', header=False, index=False)
 
 def solveBySoup():
     URL = 'https://www.ica.se/butiker/'
     URL2 = 'https://www.ica.se'
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    
+
     allLiItems = soup.findAll('li')
 
     liItemHrefValue = []
     for item in allLiItems:
         aElement = item.find('a')
         liItemHrefValue.append(aElement.get('href'))
-    
+
     #Get all store links
     storeLinks =  [URL2 + l for l in liItemHrefValue if '/butiker' in l]
 
     #Store info start at 7
     for i in range(7):
         storeLinks.pop(0)
-    
-    #Ale
-    #print(storeLinks[0])
 
-    # for link in storeLinks:
-    #     page2 = requests.get(link)
-    #     soup2 = BeautifulSoup(page2.content , 'html.parser')
-    #     store = soup2.findAll("class", class_="store-card-main-content")
-    #     print(store)
+    listOfDictionary = []
+    for storeLink in storeLinks:
+        page2 = requests.get(storeLink)
+        soup2 = BeautifulSoup(page2.content, 'html.parser')
 
-    page2 = requests.get(storeLinks[0])
-    soup2 = BeautifulSoup(page2.content, 'html.parser')
-    stores = soup2.findAll("div", class_="store-card-main-content")
-    
-    print(stores)
+        for store in soup2.findAll("div", class_="store-card-main-content"):
+            populateListOfDictionary(listOfDictionary,
+                store.find("div", class_="card-heading column size20of20 md_size2of3").text,
+                store.find("div", class_="street-name").text,
+                store.find("span", class_="zip-code").text,
+                store.find("a", class_="store-card-phone-number sprite2-p").text)
 
-    testList = []
-    for item in stores:
-        aElement = item.find("div")
-        testList.append(aElement.get('class'))
-
-    print(testList)
-
-    #storeName =  soup2.select_one('class', class_="card-heading column size20of20 md_size2of3")
-
+    pandas.DataFrame(listOfDictionary).to_excel('solveBySoup.xlsx', header=False, index=False)
 
 def main():
-    #solveByJson()
+    solveByJson()
     solveBySoup()
-   
-
-
-
-
 
 if __name__ == "__main__":
     main()
-
-   
-
-    
-    # #<div class="store-card-main-content">
-	# 		                    <header class="card-header grid_fluid no_padding">
-	# 			                    <div class="card-heading column size20of20 md_size2of3 ">ICA Kvantum Ale</div>
-	# 			                    <div class="card-open-hours column size1of3 md_lte_hidden">Öppet idag: 7-23</div>
-	# 		                    </header>
-	# 		                    <div class="store-card-content grid_fluid no_padding">
-	# 			                    <div class="column size20of20 md_size1of3">
-	# 				                    <div class="store-address">
-	# 					                    <div class="street-name">Ale Torg 7</div>
-	# 					                    <span class="zip-code">44931</span>
-	# 					                    <span>NÖDINGE</span>
-    #                                         <span class="card-open-hours card-open-hours--small md_gte_hidden">Öppet idag: 7-23</span>
-	# 				                    </div>
-    #                                     <div class="store-quick-contact">
-	# 				                        <a href="#" class="store-card-navigation-link sprite2-p" target="_blank">Hitta hit</a>
-    #                                         <a href="/Templates/Stores/Views/Pages/0303-97500" class="store-card-phone-number sprite2-p">0303-97500</a>
-    #                                     </div>
